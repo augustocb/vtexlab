182b05dd31b05f3c229016d754bc82c299755c65
Anderson Moreira <mails.amoreiras@gmail.com>
2014-04-18 18:12:54 -0300

    Cria novo arquivo de documentação

--- _plugins/layout-engine.rb
+++ _plugins/layout-engine.rb
@@ -8,6 +8,29 @@ module Jekyll
 	class LayoutEngine < Generator
 		def generate(site)
 			docFiles = parse_filesToArray("vtexlab-docs/**/*")
+			docFiles.each do |json|
+				path = File.join(Pathname(__FILE__).dirname.parent, get_docPath(json))
+				create_FilePath(path)
+
+				File.open("out.txt", "w") do |f|     
+					f.write(data_you_want_to_write)
+				end
+				#include_frontMatter(file, json)
+				
+				
+				#print("---->>>>>", File.join(Pathname(__FILE__).dirname.parent, get_docPath(json)),"\n")
+			end
+		end
+
+		def get_docPath(json)
+			return "docs/#{json['product']}/api/#{json['version']}/#{json['resource']}/index.md"
+		end
+
+		def create_FilePath(filePath)
+			path = Pathname.new(filePath)
+			if FileTest.exist?(path) == false
+				FileUtils.mkdir_p path
+			end
 		end
 
 		def parse_filesToArray(directory)
@@ -30,10 +53,31 @@ module Jekyll
 			return docFiles
 		end
 
-		def include_frontMatter(file)
+		def include_frontMatter(file, json)
 		end
 
 		def include_description(file)
+			#for docFile in docFiles
+			#	file = createFile(docFile.path)
+			#	include_frontMatter(file)
+			#	include_description(file)
+
+
+						# <div class="api-container">
+			#	<div class="api-block">
+			#	<div class="api-description">
+			#	<h1 class="doc-title">{{ page.title }}</h1>     
+			#	</div>
+			#
+			#	</div>
+			#
+			#	<div class="api-block">
+			#	<div class="api-description">
+				## Consulta quantidade em estoque2
+			#	{: #consulta-quantidade-em-estoque .slug-text }
+			#	Obtém o balanço de estoque de uma coleção de itens
+			#	POST /api/logistics/pvt/inventory/warehouseitems/getbalance
+			#	{: .api-route 
 		end
 
 	end
