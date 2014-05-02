d533670fa61046a72ff503cd0b58e8bbdf29a7b9
leandrooriente <leandroriente@gmail.com>
2014-05-02 19:34:58 -0300

    Remove assets compiled files

--- assets/javascripts/aux-navigation.js
+++
@@ -1,9 +0,0 @@
-(function(window, document){
-  $('#footer-logo').on('click', function(e){
-    $("html, body").animate({ 
-      scrollTop: 0
-    }, 700, 'easeOutCirc');
-
-    e.preventDefault();
-  });
-}(window, document));
