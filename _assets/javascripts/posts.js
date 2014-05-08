7e87b9f1138b526463a8ca99cec165d4625af024
leandrooriente <leandroriente@gmail.com>
2014-05-08 13:18:10 -0300

    FIx disqus URL

--- _assets/javascripts/posts.js
+++ _assets/javascripts/posts.js
@@ -3,6 +3,7 @@
   // DISQUS
   (function() {
     var disqus_shortname = 'vtexlab';
+    var disqus_url = POST_URL;
     var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
     dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
